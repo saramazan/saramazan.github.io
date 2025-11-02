b6bb68b463cfbfa03caacd81ac1eeefc9ddcfe7a
saramazan <sarunia.mazan@gmail.com>
2025-11-02 22:06:20 +0100

    Add files via upload

---
+++ script40.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function write(input){
+    display.value += input;
+}
+
+function clear(){
+    display.value = "";
+}
+
+function calculate(){
+
+}
